dc21db43966131c495a29db89abe2644fb2fd016
GunnarSKnudsen <waxfactor2nd@gmail.com>
2022-06-06 15:33:26 +0200

    Started analysis - Got Alpha/beta coefficients working
    In a new notebook. Don't care if it it "nooby", I really like working in notebooks...

---
+++ DD_Project/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,82 @@
+# Standard
+import pandas as pd
+import numpy as np
+import pickle
+
+# Time Cleaning
+import time
+import datetime
+from dateutil.relativedelta import relativedelta
+
+# Scraping
+import requests
+import locale
+from pandas.io.json import json_normalize
+import io
+
+from os.path import exists
+
+# Import User Defined functions
+import source.read_tickers_and_isins as URTI
+import source.get_directors_dealings as UGDD
+import source.get_timeseries as UGT
+import source.analyze_get_summary_of_data as AGSOD
+import source.preprocess_directors_dealings as UPDD
+import source.preprocess_timeseries as UPTS
+import source.preprocess_timeseries_from_excel as UPTFE
+import source.analyse_single_company as UASC
+
+if __name__ == '__main__':
+    # Constants:
+
+    # File from professor
+    filename = 'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen.xlsx'
+    timeseries_files = ['input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 1.xlsx',
+                        'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 2.xlsx',
+                        'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 3.xlsx',
+                        'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 4.xlsx',
+                        'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 5.xlsx',
+                        'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 6.xlsx',
+                        'input_data/Nasdaq Composite 16.3.2022 plus dead firms - Knudsen - RI - Part 7.xlsx'
+                        ]
+    #timeseries_files = ['input_data/smallerTestFile.xlsx', 'input_data/smallerTestFile2.xlsx']
+
+    # Locations to store stuff and stuff
+    DATA_LOCATION = 'data/'
+    DATA_LOCATION_INSIDER_RAW = DATA_LOCATION + 'raw/insider/'
+    DATA_LOCATION_INSIDER_PROCESSED = DATA_LOCATION + 'processed/insider/'
+    DATA_LOCATION_TIME_SERIES_RAW = DATA_LOCATION + 'raw/timeseries/'
+    DATA_LOCATION_TIME_SERIES_PROCESSED = DATA_LOCATION + 'processed/timeseries/'
+    DATA_LOCATION_RI = DATA_LOCATION + 'processed/RI/'
+
+    # Period of interest
+    end_time = datetime.datetime(2021, 12, 31, 23, 59, 59)
+    end_time_unix = int(time.mktime(end_time.timetuple()))
+
+    start_time = datetime.datetime(2018, 1, 1, 0, 0, 0)
+    start_time_unix = int(time.mktime(start_time.timetuple()))
+
+    # Read which companies should be analyzed
+    tickers, isins = URTI.read_tickers_and_isins(filename)
+
+    # Download raw data:
+    # UGDD.get_all_directors_dealings(DATA_LOCATION_INSIDER_RAW, tickers)
+    # # UGT.get_all_timeseries_from_isins(DATA_LOCATION_TIME_SERIES_RAW, isins, start_time_unix, end_time_unix)  # Maybe this will be of use? - But does too many requests
+    # UGT.get_all_timeseries_from_tickers(DATA_LOCATION_TIME_SERIES_RAW, tickers, start_time_unix, end_time_unix)
+
+    # Preprocess raw data
+    #UPDD.preprocess_directors_dealings(DATA_LOCATION_INSIDER_RAW, DATA_LOCATION_INSIDER_PROCESSED)
+    #UPTS.preprocess_timeseries(DATA_LOCATION_TIME_SERIES_RAW, DATA_LOCATION_TIME_SERIES_PROCESSED)
+
+    # This is no longer relevant at all!!
+    #AGSOD.analyze_generate_summary_of_data(tickers, isins, DATA_LOCATION_INSIDER_PROCESSED, DATA_LOCATION_TIME_SERIES_PROCESSED)
+
+    # Process the timeseries from Professor
+    #processed_files = UPTFE.preprocess_timeseries_from_excel(filename, timeseries_files, DATA_LOCATION_RI)
+    #print(processed_files)
+
+    # Gather Company info - Not sure if this is useful yet
+    ## Nahh skipped this part
+
+    # Start the analysis
+    UASC.analyse_single_company('US02376R1023', DATA_LOCATION_RI, DATA_LOCATION_INSIDER_PROCESSED)
---
+++ DD_Project/source/.ipynb_checkpoints/calculate_daily_returns_for_period-checkpoint.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def calculate_daily_returns_for_period(_ts, _start, _end):
+
+    # Ignore these
+    #print(_start)
+    #print(_end)
+    #print(len(_ts))
+
+    # Clone because of reasons
+    ts = _ts.copy()
+
+    # Calculate returns
+    ts['returns'] = (ts - ts.shift(1)) / ts.shift(1)
+
+    # Filter for selected period - Should do error handling here (E.g. Dead, before/After, ...)
+    filtered_ts = ts.loc[_start:_end]
+    filtered_ts = filtered_ts['returns']
+
+    #print(f"Total returns for period is {filtered_ts['returns'].sum()}")
+
+    return filtered_ts
+
+
